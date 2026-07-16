d8101869a16e81f38a4209905affec9f4176ad0b
emiliano <verde_natural2003@hotmail.com>
2026-07-16 10:52:18 -0500

    Gravity changes

--- godot_client/drm-godot-client/assets/end.gd
+++ godot_client/drm-godot-client/assets/end.gd
@@ -3,5 +3,8 @@ extends Area2D
 func _ready() -> void:
 	$AnimationPlayer.play("hover")
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: CharacterBody2D) -> void:
 	$AnimationPlayer.play("attained")
+	await get_tree().create_timer(3).timeout
+	if (body.has_method("rotate_1")):
+		body.rotate_1()
--- godot_client/drm-godot-client/scripts/character_controller.gd
+++ godot_client/drm-godot-client/scripts/character_controller.gd
@@ -1,38 +1,46 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -800.0
+const MAX_SPEED = 1000.0
+const JUMP_VELOCITY = 800.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		if up_direction.y:
+			velocity.x = direction * SPEED * (- up_direction.y) 
+		elif up_direction.x:
+			velocity.y = direction * SPEED * (up_direction.x) 
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if up_direction.y:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		elif up_direction.x:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta * up_direction.dot(Vector2.UP)
+		velocity.x += gravity * delta * up_direction.dot(Vector2.LEFT)
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		rotate_1()
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		rotate_2()
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY * up_direction.dot(Vector2.DOWN) 
+		velocity.x = JUMP_VELOCITY * up_direction.dot(Vector2.RIGHT)
+
+	
 
 	move_and_slide()
 	
-#@export var debug_run_now: bool = false:
-	#set(value):
-		#debug_run_now = false
-		#_change_to_unencrypted_resource()
-		#
-#func _change_to_unencrypted_resource():
-	## Get the path of the appropiate texture
-	#var base_dir = "res://" if OS.has_feature("editor") else OS.get_executable_path().get_base_dir()
-	#print(base_dir)
-	#var asset_path : String = "C:/Users/Emilio/Documents/GitHub/drm-project/godot_client/pikachu.png" 
-	#var err : Error = ResourceLoader.load_threaded_request(asset_path)
-	#if err != OK:
-		#printerr(name + " couldn't load asset at " + asset_path)
-	#
-	#var correct_image : CompressedTexture2D = ResourceLoader.load_threaded_get(asset_path)
-	#$Icon.texture = correct_image
+func rotate_1() -> void:
+	$AnimationPlayer.play("rotation_1")
+	up_direction = Vector2(0, 1)
+	
+func rotate_2() -> void:
+	$AnimationPlayer.play("rotation_2")
+	up_direction = Vector2(1, 0)
